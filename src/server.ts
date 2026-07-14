24c35f176ff5d56ebadc8178fa603e372e004dac
pollob debnath <pullobdebnath@gmail.com>
2026-07-14 18:59:00 +0600

    add product api route

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,63 @@ app.get("/", (req, res) => {
 //========================================== routes ===========================================
 // create user collection
 const userCollection = client.db(envVar.DB_NAME).collection("user");
+const productCollection = client.db(envVar.DB_NAME).collection("products");
+
+//  create add product route (admin - add product page)
+app.post("/api/products", async (req, res) => {
+  try {
+    const {
+      title,
+      team,
+      category,
+      shortDescription,
+      fullDescription,
+      price,
+      stock,
+      sizes,
+      imageUrl,
+    } = req.body;
+
+    // Basic validation
+    if (!title || !team || !category || !shortDescription || !fullDescription) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all required fields.",
+      });
+    }
+
+    const product = {
+      title,
+      team,
+      category,
+      shortDescription,
+      fullDescription,
+      price: Number(price),
+      stock: Number(stock),
+      sizes,
+      imageUrl,
+
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const result = await productCollection.insertOne(product);
+
+    res.status(201).json({
+      success: true,
+      message: "Product added successfully.",
+      insertedId: result.insertedId,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to add product.",
+    });
+  }
+});
+
 // create user collection route
 app.get("/api/users/me", async (req, res) => {
   try {
